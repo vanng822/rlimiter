07db740e19ecd640c6e9f69338a2fd745b242917
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2019-03-14 21:48:16 +0100

    Reformatting

--- redis.go
+++ redis.go
@@ -4,12 +4,12 @@ import "github.com/go-redis/redis"
 
 var (
 	incrementScript = redis.NewScript(`
-    local current
-    current = tonumber(redis.call("incr", KEYS[1]))
-    if current == 1 then
-        redis.call("expire", KEYS[1], ARGV[1])
-    end
-    return current
+		local current
+		current = tonumber(redis.call("incr", KEYS[1]))
+		if current == 1 then
+			redis.call("expire", KEYS[1], ARGV[1])
+		end
+		return current
   `)
 	// Client for connecting to redis database
 	Client = redis.NewClient(&redis.Options{
