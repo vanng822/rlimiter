42015cb6559a75df531db4aa3f6e1589994dbe66
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2026-02-14 10:38:45 +0100

    Introduce global prefix

--- rate.go
+++ rate.go
@@ -6,6 +6,19 @@ import (
 	"time"
 )
 
+var globalPrefix = "rlimiter:"
+
+// SetGlobalPrefix sets a global prefix for all rate limiter keys.
+// This can be useful to avoid key collisions in Redis when using multiple applications or services.
+// The prefix will be automatically suffixed with a colon if it does not already end with one.
+// Remove the global prefix if an empty string is provided.
+func SetGlobalPrefix(prefix string) {
+	if prefix != "" && prefix[len(prefix)-1] != ':' {
+		prefix += ":"
+	}
+	globalPrefix = prefix
+}
+
 // Rate define window as describe in https://redis.io/commands/incr#pattern-rate-limiter-2
 type Rate struct {
 	Window time.Duration
@@ -25,7 +38,7 @@ type rateLimiter struct {
 }
 
 func (r *rateLimiter) IncrementUsage(key string) (bool, error) {
-	name := fmt.Sprintf("%s:%s", r.prefix, key)
+	name := fmt.Sprintf("%s%s:%s", globalPrefix, r.prefix, key)
 	cmd := incrementScript.Run(context.Background(), GetClient(), []string{name}, r.rate.Window.Seconds())
 	usage, err := cmd.Int64()
 	if err != nil {
